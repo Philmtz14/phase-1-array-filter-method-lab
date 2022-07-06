b5b11cd855c0456f77c40e2ec852b398f4e7d26c
philip martinez <martinez.philip00@gmail.com>
2022-07-06 10:27:06 -0500

    commit tree 62f3c04af3604756503cdd93a5b968fa176b6a0a

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(collection, name) {
+    return collection.filter(n => name.toLowerCase() == n.toLowerCase())
+}
+
+function fuzzyMatch (collection, name){
+    return collection.filter(n => n.indexOf(name) === 0);
+}
+
+function matchName(collection, name) {
+    return collection.filter(n => n.name === name);
+}
